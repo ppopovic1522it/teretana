c78524a153ee64214f12f5d7b2a7fd501d1ac539
ppopovic1522it <ppopovic1522it@raf.rs>
2026-01-11 17:13:07 +0100

    Podesena SQLite baza i dodati seederi

---
+++ database/seeders/TrenerSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TrenerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
